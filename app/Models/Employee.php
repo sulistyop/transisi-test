af2c9fab026d0e4be063428cdce85c502fc99cd7
sulistyop <sulistyopradana59@gmail.com>
2020-12-07 14:33:05 +0700

    revisi fillable Model Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,7 +10,7 @@ class Employee extends Model
     use HasFactory;
     protected $table  = 'employees';
     protected $fillable = [
-        'name', 'company_Id',
+        'name', 'company_id',
     ];
     public function company(){
         return $this->belongsTo(Company::class);
